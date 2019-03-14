69acf0625724328b1373ddfc6759344e25297143
sunaoka <sunaoka@pocari.org>
2019-03-14 13:50:38 +0900

    add QueryLoggerServiceProviderTest

---
+++ tests/QueryLoggerServiceProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sunaoka\LaravelQueryLogger\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Sunaoka\LaravelQueryLogger\QueryLoggerServiceProvider;
+
+class QueryLoggerServiceProviderTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            QueryLoggerServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+        ]);
+    }
+
+    public function testRegister()
+    {
+        \Log::listen(function ($log) {
+            /** @var \Illuminate\Log\Events\MessageLogged $log */
+            $this->assertSame('debug', $log->level);
+            dump($log->message);
+            $this->assertRegExp("/[[0-9.]ms] select '1';/D", $log->message);
+        });
+
+        \DB::select('select ?', [1]);
+    }
+}
