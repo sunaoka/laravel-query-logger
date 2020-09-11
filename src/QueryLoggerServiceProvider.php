439d0c08d38279dec08c6bcc0e8b449f45d7e890
sunaoka <sunaoka@pocari.org>
2020-09-11 10:05:02 +0900

    Support transaction events

--- src/QueryLoggerServiceProvider.php
+++ src/QueryLoggerServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Sunaoka\LaravelQueryLogger;
 
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Events\TransactionBeginning;
+use Illuminate\Database\Events\TransactionCommitted;
+use Illuminate\Database\Events\TransactionRolledBack;
 use Illuminate\Support\ServiceProvider;
 use Psr\Log\LoggerInterface;
 
@@ -22,6 +25,21 @@ class QueryLoggerServiceProvider extends ServiceProvider
 
             $logger = app(LoggerInterface::class);
             $logger->debug(sprintf('[%sms] %s;', $query->time, vsprintf($sql, $args)));
+        });
+
+        $this->app['events']->listen(TransactionBeginning::class, function(TransactionBeginning $event) {
+            $logger = app(LoggerInterface::class);
+            $logger->debug('BEGIN;');
+        });
+
+        $this->app['events']->listen(TransactionCommitted::class, function(TransactionCommitted $event) {
+            $logger = app(LoggerInterface::class);
+            $logger->debug('COMMIT;');
+        });
+
+        $this->app['events']->listen(TransactionRolledBack::class, function(TransactionRolledBack $event) {
+            $logger = app(LoggerInterface::class);
+            $logger->debug('ROLLBACK;');
         });
     }
 }
