8cc9ef25b6ab9725869863a5a44625d3d6f00b9e
sunaoka <sunaoka@pocari.org>
2023-12-06 15:06:56 +0900

    Drop support substituteBindingsIntoRawSql()

--- src/QueryLoggerServiceProvider.php
+++ src/QueryLoggerServiceProvider.php
@@ -20,40 +20,35 @@ class QueryLoggerServiceProvider extends ServiceProvider
      */
     public function boot(LoggerInterface $logger)
     {
-        $this->app['db']->listen(function(QueryExecuted $query) use ($logger) {
-            if (version_compare($this->app->version(), '10.15.0') >= 0) {
-                $sql = $query->connection->getQueryGrammar()
-                    ->substituteBindingsIntoRawSql($query->sql, $query->connection->prepareBindings($query->bindings));
-            } else {
-                $bindings = $query->connection->prepareBindings($query->bindings);
+        $this->app['db']->listen(function (QueryExecuted $query) use ($logger) {
+            $bindings = $query->connection->prepareBindings($query->bindings);
 
-                $args = [];
-                foreach ($bindings as $binding) {
-                    if ($binding === null) {
-                        $args[] = 'null';
-                    } elseif (is_float($binding) || is_int($binding)) {
-                        $args[] = $binding;
-                    } else {
-                        $args[] = $query->connection->getPdo()->quote($binding);
-                    }
+            $args = [];
+            foreach ($bindings as $binding) {
+                if ($binding === null) {
+                    $args[] = 'null';
+                } elseif (is_float($binding) || is_int($binding)) {
+                    $args[] = $binding;
+                } else {
+                    $args[] = $query->connection->getPdo()->quote($binding);
                 }
-
-                $sql = str_replace(['%', '?', '%s%s'], ['%%', '%s', '??'], $query->sql);
-                $sql = vsprintf($sql, $args);
             }
+
+            $sql = str_replace(['%', '?', '%s%s'], ['%%', '%s', '??'], $query->sql);
+            $sql = vsprintf($sql, $args);
 
             $logger->debug(sprintf('[%sms] %s;', $query->time, $sql));
         });
 
-        $this->app['events']->listen(TransactionBeginning::class, function(TransactionBeginning $event) use ($logger) {
+        $this->app['events']->listen(TransactionBeginning::class, function (TransactionBeginning $event) use ($logger) {
             $logger->debug('BEGIN;');
         });
 
-        $this->app['events']->listen(TransactionCommitted::class, function(TransactionCommitted $event) use ($logger) {
+        $this->app['events']->listen(TransactionCommitted::class, function (TransactionCommitted $event) use ($logger) {
             $logger->debug('COMMIT;');
         });
 
-        $this->app['events']->listen(TransactionRolledBack::class, function(TransactionRolledBack $event) use ($logger) {
+        $this->app['events']->listen(TransactionRolledBack::class, function (TransactionRolledBack $event) use ($logger) {
             $logger->debug('ROLLBACK;');
         });
     }
